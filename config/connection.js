892e34e1f50d37f84d3c6e747d936e0b9d78b6a6
apark0819 <apark0819@gmail.com>
2022-09-09 01:30:10 -0700

    model index

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+//Execute query
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+module.exports = mongoose.connection;
